6cc68c6d842a7a13a3b5fad430e58b53a104a911
Matthias Riegel <public@verriegelt.net>
2024-08-06 15:43:28 +0200

    fixed docker compose alias

--- home-manager/docker/default.nix
+++ home-manager/docker/default.nix
@@ -3,7 +3,7 @@
   home.shellAliases = {
     dcud = "sudo docker compose up -d";
     dcd = "sudo docker compose down";
-    dcr = "sudo docker compose down && docker-compose up -d";
+    dcr = "sudo docker compose down && docker compose up -d";
     dcl = "sudo docker compose logs";
   };
 }
