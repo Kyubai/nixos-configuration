460dc3781f51766d4b0736f7b0b5c79da9af3b6e
Matthias Riegel <public@verriegelt.net>
2024-10-09 14:55:41 +0200

    added remmina to desktop packages

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -27,16 +27,15 @@ in {
       })
       vivaldi-ffmpeg-codecs
       virtiofsd # for virt-manager https://discourse.nixos.org/t/virt-manager-cannot-find-virtiofsd/26752
+      remmina # rdp client
       signal-desktop
       syncthing
-      # spice_gtk # required for virt-manager
     ];
 
     # required for virt-manager
     # https://nixos.wiki/wiki/Virt-manager
     virtualisation.libvirtd.enable = true;
     programs.virt-manager.enable = true;
-    # security.wrappers.spice-client-glib-usb-acl-helper.source = "${pkgs.spice_gtk}/bin/spice-client-glib-usb-acl-helper";
     virtualisation.spiceUSBRedirection.enable = true;
 
     virtualisation.virtualbox.host.enable = true;
