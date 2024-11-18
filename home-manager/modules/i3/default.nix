ccc16920deec864309e1087985aec69f81ecd30e
Matthias Riegel <public@verriegelt.net>
2024-11-18 18:56:58 +0100

    fixed i3 not using modules definitions

--- home-manager/modules/i3/default.nix
+++ home-manager/modules/i3/default.nix
@@ -14,13 +14,14 @@ in {
 
   config = lib.mkIf cfgI3.enable {
     modules.kitty.enable = true; # kitty is used as terminal emulator
+
     xsession.windowManager.i3 = {
       enable = true;
       config = {
-        modifier = cfgI3.modifier;
+        modifier = config.modules.i3.modifier;
         terminal = "${pkgs.kitty}/bin/kitty";
         keybindings = lib.mkOptionDefault {
-          "${cfgI3.modifier}+semicolon" = "exec ${config.xsession.windowManager.i3.config.terminal}"; # 47 is Semicolon
+          "${config.modules.i3.modifier}+semicolon" = "exec ${config.xsession.windowManager.i3.config.terminal}"; # 47 is Semicolon
         };
         window = {
           titlebar = false;
