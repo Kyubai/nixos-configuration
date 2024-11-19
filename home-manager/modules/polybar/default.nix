49e9739cae73ce62ad3d034694beaaa678fb53c9
Matthias Riegel <public@verriegelt.net>
2024-11-19 12:47:50 +0100

    fixed polybar not running, since I didn't enable the service, lol

--- home-manager/modules/polybar/default.nix
+++ home-manager/modules/polybar/default.nix
@@ -6,8 +6,13 @@
   cfgPolybar = config.modules.polybar;
 in {
   options.modules.polybar.enable = lib.mkEnableOption "enable polybar config";
-
   config = lib.mkIf cfgPolybar.enable {
+
+    services.polybar = {
+      enable = true;
+      script = "polybar bar &";
+    };
+
     xsession.windowManager.i3.config.bars = [
       {
         statusCommand = "polybar";
