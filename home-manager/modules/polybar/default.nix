546a99490e23f8dd67c00e63380e5f9010173dbb
Matthias Riegel <public@verriegelt.net>
2024-11-19 13:08:58 +0100

    fixed polybar binary not being found by i3

--- home-manager/modules/polybar/default.nix
+++ home-manager/modules/polybar/default.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: let
   cfgPolybar = config.modules.polybar;
@@ -11,11 +12,21 @@ in {
     services.polybar = {
       enable = true;
       script = "polybar bar &";
+      settings = {
+        "module/volume" = {
+          type = "internal/pulseaudio";
+          format.volume = " ";
+          label.muted.text = "🔇";
+          label.muted.foreground = "#666";
+          ramp.volume = ["🔈" "🔉" "🔊"];
+          click.right = "pavucontrol &";
+        };
+      };
     };
 
     xsession.windowManager.i3.config.bars = [
       {
-        statusCommand = "polybar";
+        statusCommand = "${pkgs.polybar}/bin/polybar";
       }
     ];
   };
