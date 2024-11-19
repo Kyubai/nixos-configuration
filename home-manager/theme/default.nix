117de1cb7b2ab0ed1d190af36a8b57c925bf50e6
Matthias Riegel <public@verriegelt.net>
2024-11-19 03:02:08 +0100

    deleted themes

---
+++ home-manager/theme/default.nix
@@ -0,0 +1,24 @@
+{ pkgs,
+  ...
+}:
+{
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Adwaita-dark";
+      package = pkgs.gnome.gnome-themes-extra;
+    };
+  };
+
+  qt = {
+    enable = true;
+    platformTheme.name = "breeze";
+    style.name = "adwaita-dark";
+  };
+
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+  };
+}
