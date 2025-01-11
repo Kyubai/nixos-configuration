82573ccd68c2363580b113cfed675a82eefcef38
Matthias Riegel <public@verriegelt.net>
2025-01-11 14:42:42 +0100

    added fortivpn to security modules and reworked rdp/kerberos support

--- modules/it-sec.nix
+++ modules/it-sec.nix
@@ -6,12 +6,16 @@
 }:
 with lib; let
   cfgSec = config.modules.sec.utils;
+  # freerdp_custom = pkgs.lib.overrideDerivation (pkgs.freerdp3) (old: {NIX_CFLAGS_COMPILE = " -D_WITH_KRB5=ON";});
 in {
   options.modules.sec.utils.enable = mkEnableOption "basic dfir and pentest utils";
   config = mkIf cfgSec.enable {
     environment.systemPackages = with pkgs; [
       nmap
       lnav
+      openfortivpn
     ];
+    security.krb5.enable = true;
+    security.krb5.settings.include = "/current_customer/.config/krb5.conf";
   };
 }
