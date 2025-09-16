f4d6d424031013af1bfbb69c09a20523a60bfadc
Matthias Riegel <public@verriegelt.net>
2025-09-16 13:14:39 +0200

    fixed dns settings

--- outputs/systems/work2home/configuration.nix
+++ outputs/systems/work2home/configuration.nix
@@ -37,28 +37,6 @@
   # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
   networking.hostName = "work2home";
 
-  services.dnsmasq = {
-    enable = true;
-    settings = {
-      listen-address = "::1,127.0.0.1";
-      no-resolv = true; # ignore resolv.conf
-      log-queries = true;
-      log-facility = "/var/log/dnsmasq.log";
-      server = [
-        "185.222.222.222" # dns.sb
-        "45.11.45.11" # dns.sb
-        # "193.110.81.0" # dns0.eu
-        # "2a0f:fc80::" # dns0.eu
-        # "185.253.5.0" # dns0.eu
-        # "2a0f:fc81::" # dns0.eu
-        "/addyet.intern/10.10.0.2" # Hacker DMZ
-        "/addyet.intern/192.168.250.5" # addyet.intern
-        "/verriegelt.net/10.105.201.1"
-        "/intern.verriegelt.net/10.105.201.1"
-      ];
-    };
-  };
-
   fileSystems."/data/tools/personal" = {
     device = ".host:/data_personal";
     fsType = "fuse./run/current-system/sw/bin/vmhgfs-fuse";
