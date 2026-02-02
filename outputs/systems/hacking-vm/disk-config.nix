2dd7a9e994a5b7a47c0f25a40a66f28fceda1c2d
Matthias Riegel <public@verriegelt.net>
2026-02-02 13:50:52 +0100

    added swap

--- outputs/systems/hacking-vm/disk-config.nix
+++ outputs/systems/hacking-vm/disk-config.nix
@@ -38,6 +38,12 @@
       pool = {
         type = "lvm_vg";
         lvs = {
+          swap = {
+            size = "20%FREE";
+            content = {
+              type = "swap";
+            };
+          };
           root = {
             size = "100%FREE";
             content = {
